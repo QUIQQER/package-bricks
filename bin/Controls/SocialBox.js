c6e689d24ed88cfcc7d37c7b0375c3d6efdf9d21
Henning Leutz <leutz@pcsg.de>
2015-03-06 20:14:02 +0100

    social box

---
+++ bin/Controls/SocialBox.js
@@ -0,0 +1,45 @@
+
+/**
+ *
+ */
+
+define('package/quiqqer/bricks/bin/Controls/SocialBox', [
+
+    'qui/controls/elements/FormList',
+
+    'css!package/quiqqer/bricks/bin/Controls/SocialBox.css'
+
+], function(QUIFormList)
+{
+    "use strict";
+
+    return new Class({
+
+        Extends : QUIFormList,
+        Type    : 'package/quiqqer/bricks/bin/Controls/SocialBox',
+
+        initialize : function(options)
+        {
+            this.parent( options );
+
+            this.setAttribute(
+                'entry',
+
+                '<div class="quiqqer-bricks-socialBox-entry">' +
+                    '<label>' +
+                        '<span>Url</span>' +
+                        '<input type="text" name="url" />' +
+                    '</label>' +
+                    '<label>' +
+                        '<span>Icon</span>' +
+                        '<input type="text" name="icon" />' +
+                    '</label>' +
+                    '<label>' +
+                        '<span>Hintergrundfarbe</span>' +
+                        '<input type="text" name="background" />' +
+                    '</label>' +
+                '</div>'
+            );
+        }
+    });
+});
