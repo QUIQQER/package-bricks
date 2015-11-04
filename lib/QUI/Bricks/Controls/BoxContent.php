3dec49da57f6dda2fd8715642a277f7e4133752b
Michael <michael.danielczok@gmail.com>
2015-11-04 16:04:25 +0100

    Neuer Brick - Text wird in Boxen angezeigt.

---
+++ lib/QUI/Bricks/Controls/BoxContent.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file contains QUI\Bricks\Controls\BoxContent
+ */
+
+namespace QUI\Bricks\Controls;
+
+use QUI;
+
+/**
+ * Class BoxContent
+ *
+ * @package quiqqer/bricks
+ */
+class BoxContent extends QUI\Control
+{
+    /**
+     * constructor
+     *
+     * @param Array $attributes
+     */
+    public function __construct($attributes = array())
+    {
+        // default options
+        $this->setAttributes(array(
+            'title'      => 'Box Content',
+            'contentList' => false,
+            'entries' => array()
+        ));
+
+        parent::__construct($attributes);
+
+        $this->addCSSFile(
+            dirname(__FILE__).'/BoxContent.css'
+        );
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+        $entries = $this->getAttribute('entries');
+
+        if (is_string($entries)) {
+            $entries = json_decode($entries, true);
+        }
+
+        $Engine->assign(array(
+            'this'       => $this,
+            'entries' => $entries
+        ));
+
+
+        return $Engine->fetch(dirname(__FILE__).'/BoxContent.html');
+    }
+}
