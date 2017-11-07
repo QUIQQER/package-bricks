7e8bc9c0ee3dbfa0406be1a38d3f178f978e4a6d
Jan Wennrich <jan@pcsg.de>
2017-11-07 17:44:47 +0100

    fix: content-switch brick entry settings use locale variables

--- bin/Controls/ContentSwitcher.js
+++ bin/Controls/ContentSwitcher.js
@@ -10,10 +10,14 @@ define('package/quiqqer/bricks/bin/Controls/ContentSwitcher', [
 
     'qui/controls/elements/FormList',
 
+    'Locale',
+
     'css!package/quiqqer/bricks/bin/Controls/ContentSwitcher.css'
 
-], function (QUIFormList) {
+], function (QUIFormList, QUILocale) {
     "use strict";
+
+    var lg = 'quiqqer/bricks';
 
     return new Class({
 
@@ -28,15 +32,21 @@ define('package/quiqqer/bricks/bin/Controls/ContentSwitcher', [
 
             '<div class="quiqqer-bricks-ContentSwitcher-entry">' +
                 '<label>' +
-                    '<span class="entry-title">Bild</span>' +
+                    '<span class="entry-title">'+
+                        QUILocale.get(lg, 'contentSwitcher.entries.entry.picture') +
+                    '</span>' +
                     '<input type="text" class="media-image" name="img" />' +
                 '</label>' +
                 '<label>' +
-                    '<span class="entry-title">Überschrift</span>' +
+                    '<span class="entry-title">'+
+                        QUILocale.get(lg, 'contentSwitcher.entries.entry.title') +
+                    '</span>' +
                     '<input type="text" name="title" />' +
                 '</label>' +
                 '<label>' +
-                    '<span class="entry-title">Text</span>' +
+                    '<span class="entry-title">'+
+                        QUILocale.get(lg, 'contentSwitcher.entries.entry.content') +
+                    '</span>' +
                     '<textarea name="content" rows="10"></textarea>' +
                 '</label>' +
             '</div>'
