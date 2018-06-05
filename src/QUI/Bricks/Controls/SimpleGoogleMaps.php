e158dc4a7852925a16fc81cc6dba3b77f270b3cd
michael.daniel <michael@pcsg.de>
2018-06-05 15:29:59 +0200

    feat: Setting to prevent Google Maps auto load.

--- src/QUI/Bricks/Controls/SimpleGoogleMaps.php
+++ src/QUI/Bricks/Controls/SimpleGoogleMaps.php
@@ -20,20 +20,19 @@ class SimpleGoogleMaps extends QUI\Control
      *
      * @param array $attributes
      */
-    public function __construct($attributes = array())
+    public function __construct($attributes = [])
     {
         // default options
-        $this->setAttributes(array(
-            'title' => ''
-        ));
+        $this->setAttributes([
+            'title'          => '',
+            'preventLoadMap' => false
+        ]);
 
         parent::__construct($attributes);
 
         $this->addCSSFile(
             dirname(__FILE__) . '/SimpleGoogleMaps.css'
         );
-
-//        $this->setAttribute('qui-class', "package/quiqqer/bricks/bin/Controls/SimpleGoogleMaps");
     }
 
     /**
@@ -43,28 +42,40 @@ class SimpleGoogleMaps extends QUI\Control
      */
     public function getBody()
     {
-        $Engine      = QUI::getTemplateManager()->getEngine();
-        $brickPlace  = $this->getAttribute('place');
-        $brickZip    = $this->getAttribute('zip');
-        $brickStreet = $this->getAttribute('street');
-        $brickCity   = $this->getAttribute('city');
-        $zoom        = $this->getAttribute('zoom');
+        $Engine         = QUI::getTemplateManager()->getEngine();
+        $brickPlace     = $this->getAttribute('place');
+        $brickZip       = $this->getAttribute('zip');
+        $brickStreet    = $this->getAttribute('street');
+        $brickCity      = $this->getAttribute('city');
+        $zoom           = $this->getAttribute('zoom');
+        $preventLoadMap = $this->getAttribute('preventLoadMap');
+        $imgUrl         = '';
 
         if (!$zoom) {
             $zoom = 15;
         }
 
-        $query = http_build_query(array(
+        $query = http_build_query([
             'key' => trim($this->getAttribute('api')),
             'q'   => "{$brickPlace},{$brickZip},{$brickStreet},{$brickCity}"
-        ));
+        ]);
 
         $url = 'https://www.google.com/maps/embed/v1/place?' . $query . "&zoom=" . $zoom;
+        if ($preventLoadMap) {
+            $imgUrl = URL_OPT_DIR . 'quiqqer/bricks/bin/images/SimpleGoogleMapsBackground1.png';
 
-        $Engine->assign(array(
-            'this' => $this,
-            'url'  => $url
-        ));
+            $this->setAttributes([
+                'qui-class'       => "package/quiqqer/bricks/bin/Controls/SimpleGoogleMaps",
+                'data-qui-url'    => $url,
+                'data-qui-imgUrl' => $imgUrl
+            ]);
+        }
+
+        $Engine->assign([
+            'this'           => $this,
+            'url'            => $url,
+            'preventLoadMap' => $preventLoadMap
+        ]);
 
         return $Engine->fetch(dirname(__FILE__) . '/SimpleGoogleMaps.html');
     }
