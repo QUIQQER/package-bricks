22266cf6fb7920e6324c66f15080d1b1ee9a3da1
Jan Wennrich <jan@pcsg.de>
2018-07-18 11:06:13 +0200

    fix: corrected strict type check conversion error

--- bin/Controls/Slider/PromosliderSettings.js
+++ bin/Controls/Slider/PromosliderSettings.js
@@ -560,7 +560,7 @@ define('package/quiqqer/bricks/bin/Controls/Slider/PromosliderSettings', [
                     Type.value        = data.type;
                     Url.value         = data.url;
 
-                    if (data.newTab === 1) {
+                    if (data.newTab === "1") {
                         Dialog.NewTabSwitch.on();
                     } else {
                         Dialog.NewTabSwitch.off();
