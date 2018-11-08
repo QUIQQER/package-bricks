e47923d83a9d5f95eab27a19a0da18f6a3bf83a6
michael.daniel <michael@pcsg.de>
2018-11-08 14:11:39 +0100

    fix: refactor: integrate setProject method, because editor needs project object to work properly

--- bin/Controls/CustomerReviews.js
+++ bin/Controls/CustomerReviews.js
@@ -27,39 +27,53 @@ define('package/quiqqer/bricks/bin/Controls/CustomerReviews', [
         initialize: function (options) {
             this.parent(options);
 
+            this.$Project = null;
+
             this.addEvents({
-                onParsed  : this.$onParsed
+                onParsed: this.$onParsed
             });
 
             this.setAttributes({
-                    buttonText: QUILocale.get(lg, 'customerReviews.reviews.addButton'),
-                entry: '<div class="quiqqer-bricks-ContentSwitcher-entry" style="display: none;">' +
-                           '<label class="entry-title">' +
-                               '<span class="entry-title">' +
-                                 QUILocale.get(lg, 'customerReviews.reviews.entry.avatar') +
-                               '</span>' +
-                               '<input class="media-image" data-qui-options-selectable_types="image" name="userImage"/>' +
-                           '</label>' +
-                           '<label>' +
-                               '<span class="entry-title">' +
-                                 QUILocale.get(lg, 'customerReviews.reviews.entry.userName') +
-                               '</span>' +
-                               '<input type="text" name="userName" placeholder="' + QUILocale.get(lg, 'customerReviews.reviews.entry.userName.placeholder') + '"/>' +
-                           '</label>' +
-                           '<label>' +
-                               '<span class="entry-title">' +
-                                    QUILocale.get(lg, 'customerReviews.reviews.entry.userTitle') +
-                               '</span>' +
-                               '<input type="text" name="userTitle" placeholder="' + QUILocale.get(lg, 'customerReviews.reviews.entry.userTitle.placeholder') + '" />' +
-                            '</label>' +
-                           '<label>' +
-                               '<span class="entry-title">' +
-                                  QUILocale.get(lg, 'customerReviews.reviews.entry.content') +
-                               '</span>' +
-                               '<input name="content" class="field-container-field field-description" data-qui="controls/editors/Input" />' +
-                           '</label>' +
-                       '</div>'
+                buttonText: QUILocale.get(lg, 'customerReviews.reviews.addButton'),
+                entry     : '<div class="quiqqer-bricks-ContentSwitcher-entry" style="display: none;">' +
+                    '<label class="entry-title">' +
+                    '<span class="entry-title">' +
+                    QUILocale.get(lg, 'customerReviews.reviews.entry.avatar') +
+                    '</span>' +
+                    '<input class="media-image" data-qui-options-selectable_types="image" name="avatar"/>' +
+                    '</label>' +
+                    '<label>' +
+                    '<span class="entry-title">' +
+                    QUILocale.get(lg, 'customerReviews.reviews.entry.userName') +
+                    '</span>' +
+                    '<input type="text" name="userName" placeholder="' + QUILocale.get(lg,
+                        'customerReviews.reviews.entry.userName.placeholder') + '"/>' +
+                    '</label>' +
+                    '<label>' +
+                    '<span class="entry-title">' +
+                    QUILocale.get(lg, 'customerReviews.reviews.entry.userTitle') +
+                    '</span>' +
+                    '<input type="text" name="userTitle" placeholder="' + QUILocale.get(lg,
+                        'customerReviews.reviews.entry.userTitle.placeholder') + '" />' +
+                    '</label>' +
+                    '<label>' +
+                    '<span class="entry-title">' +
+                    QUILocale.get(lg, 'customerReviews.reviews.entry.content') +
+                    '</span>' +
+                    '<input name="content" class="field-container-field field-description" data-qui="controls/editors/Input" />' +
+                    '</label>' +
+                    '</div>'
             });
+        },
+
+        /**
+         * set the project to the control
+         *
+         * @param Project
+         */
+        setProject: function (Project) {
+            this.$Project = Project;
+            this.$onParsed(false, this.getElm());
         },
 
         /**
@@ -68,12 +82,36 @@ define('package/quiqqer/bricks/bin/Controls/CustomerReviews', [
          * Fired after (inherited) FormList has parsed the content
          *
          * @param event
-         * @param Element - The element that was previously parsed by (inherited) FormList
+         * @param Node - The element that was previously parsed by (inherited) FormList
          */
-        $onParsed: function (event, Element) {
-            QUIControls.parse(Element).then(function () {
+        $onParsed: function (event, Node) {
+            if (!this.$Project) {
+                return;
+            }
+
+            this.$executeParsing(Node);
+        },
+
+        /**
+         * Parse the editor
+         *
+         * @param Node
+         * @returns {Promise}
+         */
+        $executeParsing: function (Node) {
+            var self = this;
+
+            return QUIControls.parse(Node).then(function () {
                 // Element is fully parsed so we can finally show it
-                Element.getElement('.quiqqer-bricks-ContentSwitcher-entry').show();
+                Node.getElements('.quiqqer-bricks-ContentSwitcher-entry').show();
+
+                var inputEditors = Node.getElements('[data-qui="controls/editors/Input"]').map(function (InnerNode) {
+                    return QUI.Controls.getById(InnerNode.get('data-quiid'));
+                });
+
+                for (var i = 0, len = inputEditors.length; i < len; i++) {
+                    inputEditors[i].setProject(self.$Project);
+                }
             });
         }
     });
