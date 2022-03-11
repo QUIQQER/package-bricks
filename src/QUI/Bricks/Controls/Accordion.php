2da948c15373ec146f273110c61690d1524364d2
michael.danielczok <michael@pcsg.de>
2022-03-11 21:45:40 +0100

    feat: Use FAQ schema markup [Accordion].

--- src/QUI/Bricks/Controls/Accordion.php
+++ src/QUI/Bricks/Controls/Accordion.php
@@ -16,6 +16,16 @@ use QUI;
 class Accordion extends QUI\Control
 {
     /**
+     * [
+     *   'entryTitle'   => string,
+     *   'entryContent' => string
+     * ]
+     *
+     * @var array
+     */
+    protected array $entries = [];
+
+    /**
      * constructor
      *
      * @param array $attributes
@@ -24,13 +34,14 @@ class Accordion extends QUI\Control
     {
         // default options
         $this->setAttributes([
-            'nodeName'     => 'section',
-            'class'        => 'quiqqer-accordion',
-            'qui-class'    => 'package/quiqqer/bricks/bin/Controls/Accordion',
-            'stayOpen'     => false, // if true make accordion items stay open when another item is opened
-            'openFirst'    => false, // the first entry is initially opened
-            'listMaxWidth' => 0, // positive numbers only, 0 disabled this option.
-            'entries'      => []
+            'nodeName'             => 'section',
+            'class'                => 'quiqqer-accordion',
+            'qui-class'            => 'package/quiqqer/bricks/bin/Controls/Accordion',
+            'stayOpen'             => false, // if true make accordion items stay open when another item is opened
+            'openFirst'            => false, // the first entry is initially opened
+            'listMaxWidth'         => 0, // positive numbers only, 0 disabled this option.
+            'entries'              => [],
+            'useFaqStructuredData' => false
         ]);
 
         parent::__construct($attributes);
@@ -64,13 +75,34 @@ class Accordion extends QUI\Control
             $entries = json_decode($entries, true);
         }
 
+        $this->entries = $entries;
+
         $Engine->assign([
-            'this'         => $this,
-            'openFirst'    => $this->getAttribute('openFirst'),
-            'listMaxWidth' => $maxWidth,
-            'entries'      => $entries
+            'this'                 => $this,
+            'openFirst'            => $this->getAttribute('openFirst'),
+            'listMaxWidth'         => $maxWidth,
+            'entries'              => $this->entries,
+            'useFaqStructuredData' => $this->getAttribute('useFaqStructuredData')
         ]);
 
         return $Engine->fetch(dirname(__FILE__).'/Accordion.html');
+    }
+
+    /**
+     * Generate JSON-LD FAQ Schema Code
+     *
+     * @return string
+     * @throws QUI\Exception
+     */
+    public function createJSONLDFAQSchemaCode()
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+
+        $Engine->assign([
+            'this'    => $this,
+            'entries' => $this->entries
+        ]);
+
+        return $Engine->fetch(dirname(__FILE__).'/Accordion.JSON-LD-Schema.html');
     }
 }
