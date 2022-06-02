18ba97f7352ab230086a1a0fdda665a2d1eadf7e
Campii <dominik.chrzanowski183@gmail.com>
2022-06-02 12:09:45 +0200

    feat: php loads html and css depending on the template [CustomerReviewsSlider] quiqqer/package-bricks#134

--- src/QUI/Bricks/Controls/Slider/CustomerReviewsSlider.php
+++ src/QUI/Bricks/Controls/Slider/CustomerReviewsSlider.php
@@ -12,31 +12,41 @@ use QUI;
 /**
  * Class CustomerReviews
  *
- * @package quiqqer/bricks
+ * @package quiqqer/bricks/Controls
  */
-class CustomerReviewsSlider extends QUI\Control
+class CustomerReviewsSlider extends AbstractPromoslider
 {
-    /**
-     * constructor
-     *
-     * @param array $attributes
-     */
     public function __construct($attributes = [])
     {
         // default options
         $this->setAttributes([
-            'template'   => 'default'
+            'data-qui' => 'package/quiqqer/bricks/bin/Controls/Slider/CustomerReviewsSlider',
+            'template' => 'default',
         ]);
 
         parent::__construct($attributes);
     }
 
-    /**
-     * (non-PHPdoc)
-     *
-     * @see \QUI\Control::create()
-     */
+
     public function getBody()
     {
+        $Engine = QUI::getTemplateManager()->getEngine();
+
+        $entries = json_decode($this->getAttribute('entries'), true);
+        $template = $this->getAttribute('template');
+        $path = \dirname(__FILE__) . '/CustomerReviewsSlider.' . $template . '.html';
+
+        $options = [
+            'this'    => $this,
+            'entries' => $entries
+        ];
+
+        $this->addCSSFile(
+            \dirname(__FILE__) . '/CustomerReviewsSlider.' . $template . '.css'
+        );
+
+        $Engine->assign($options);
+
+        return $Engine->fetch($path);
     }
 }
