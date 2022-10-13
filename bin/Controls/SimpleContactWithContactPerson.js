e7efcb7f990ca013a210fa9e7888b52a6b209030
Campii <dominik.chrzanowski183@gmail.com>
2022-10-13 13:49:45 +0200

    feat: Initizalizin new brick, added new template [SimpleContactWithContactPerson] quiqqer/package-bricks#138

---
+++ bin/Controls/SimpleContactWithContactPerson.js
@@ -0,0 +1,184 @@
+/**
+ * QUIQQER Contact Control
+ *
+ * @author www.pcsg.de (Dominik Chrzanowski)
+ * @module Bricks\Controls\SimpleContactWithContactPerson
+ */
+define('package/quiqqer/bricks/bin/Controls/SimpleContactWithContactPerson', [
+
+    'qui/QUI',
+    'qui/controls/Control',
+    'utils/Controls',
+    'qui/controls/loader/Loader',
+    'Ajax',
+    'Locale'
+
+], function (QUI, QUIControl, QUIControlUtils, QUILoader, Ajax, QUILocale) {
+    "use strict";
+
+    var lg = 'quiqqer/bricks';
+
+    return new Class({
+
+        Extends: QUIControl,
+        Type   : 'Controls/SimpleContact',
+
+        Binds: [
+            '$onImport'
+        ],
+
+        initialize: function (options) {
+            this.parent(options);
+
+            this.Loader = new QUILoader();
+
+            this.$Text            = null;
+            this.$Email           = null;
+            this.$Name            = null;
+            this.$captchaResponse = false;
+            this.$captchaRequired = false;
+
+            this.addEvents({
+                onImport: this.$onImport
+            });
+        },
+
+        /**
+         * event : on import
+         */
+        $onImport: function () {
+            var self = this;
+
+            this.Loader.inject(this.$Elm);
+            this.Form = this.$Elm.getElement('form');
+
+            var Button = this.Form.getElement('.quiqqer-simpleContactPerson-button');
+
+            if (Button) {
+                Button.addEvent('click', function (event) {
+                    event.stop();
+
+                    if (self.$captchaRequired && !self.$captchaResponse) {
+                        QUI.getMessageHandler(function (MH) {
+                            MH.options.displayTimeMessages = 2000;
+
+                            var CaptchaElm = self.$Elm.getElement('.qui-contact-captcha');
+
+                            if (!CaptchaElm) {
+                                CaptchaElm = undefined;
+                            }
+
+                            MH.addError(
+                                QUILocale.get(lg, 'brick.control.simpleContact.error.captcha_failed'),
+                                CaptchaElm
+                            );
+                        });
+
+                        return;
+                    }
+
+                    self.$Elm.getElement('form').fireEvent('submit');
+                });
+                Button.removeAttribute('disabled');
+            }
+
+            this.$Elm.getElement('form').addEvent('submit', function (event) {
+                if (typeof event !== 'undefined') {
+                    event.stop();
+                }
+
+                self.send();
+            });
+
+            this.$Text  = this.$Elm.getElement('[name="message"]');
+            this.$Email = this.$Elm.getElement('[name="email"]');
+            this.$Name  = this.$Elm.getElement('[name="name"]');
+
+            // CAPTCHA
+            var CaptchaElm = this.$Elm.getElement(
+                'div[data-qui="package/quiqqer/captcha/bin/controls/CaptchaDisplay"]'
+            );
+
+            if (!CaptchaElm) {
+                return;
+            }
+
+            QUIControlUtils.getControlByElement(CaptchaElm).then(function (CaptchaDisplay) {
+                CaptchaDisplay.getCaptchaControl().then(function (CaptchaControl) {
+                    self.$captchaRequired = true;
+
+                    CaptchaControl.addEvents({
+                        onSuccess: function (response) {
+                            self.$captchaResponse = response;
+                        },
+                        onExpired: function () {
+                            self.$captchaResponse = false;
+                        }
+                    });
+                });
+            });
+        },
+
+        /**
+         * Send contact message
+         */
+        send: function () {
+
+            if (this.$Text.value === '') {
+                this.$Text.focus();
+                return;
+            }
+
+            if (this.$Email.value === '') {
+                this.$Email.focus();
+                return;
+            }
+
+            if (this.$Name.value === '') {
+                this.$Name.focus();
+                return;
+            }
+
+            var PrivacyPolicyCheckbox = this.$Elm.getElement('#qui-simpleContactPerson-privacypolicy__standard');
+            var privacyPolicyAccepted = false;
+
+            if (PrivacyPolicyCheckbox) {
+                privacyPolicyAccepted = PrivacyPolicyCheckbox.checked;
+            }
+
+            var self = this;
+
+            this.Loader.show();
+
+            Ajax.post('package_quiqqer_bricks_ajax_contact', function (result) {
+                if (result) {
+                    var html = '<span class="fa fa-check fa-check-simple-contact control-color"></span>';
+                    html += QUILocale.get('quiqqer/bricks', 'brick.control.simpleContact.successful');
+                    self.Form.set('html', html);
+                }
+
+                self.Loader.hide();
+
+            }, {
+                'package'            : 'quiqqer/bricks',
+                brickId              : this.$Elm.get('data-brickid'),
+                message              : this.$Text.value,
+                email                : this.$Email.value,
+                name                 : this.$Name.value,
+                showError            : false,
+                project              : JSON.encode(QUIQQER_PROJECT),
+                siteId               : QUIQQER_SITE.id,
+                privacyPolicyAccepted: privacyPolicyAccepted ? 1 : 0,
+                captchaResponse      : this.$captchaResponse,
+                onError              : function (Exception) {
+                    self.Loader.hide();
+
+                    QUI.getMessageHandler(function (MH) {
+                        MH.options.displayTimeMessages = 8000;
+                        MH.addError(Exception.getMessage(), self.$Elm);
+                    });
+                }
+            });
+        }
+    });
+});
---
+++ src/QUI/Bricks/Controls/SimpleContactWithContactPerson.php
@@ -0,0 +1,321 @@
+<?php
+
+/**
+ * This file contains \QUI\Bricks\Controls\SimpleContactWithContactPerson
+ */
+
+namespace QUI\Bricks\Controls;
+
+use QUI;
+use QUI\Captcha\Handler as CaptchaHandler;
+
+/**
+ * Mini contact control
+ * {control control="\QUI\Bricks\Controls\SimpleContact" labels=false}
+ *
+ * @author  www.pcsg.de (Dominik Chrzanowski)
+ */
+class SimpleContactWithContactPerson extends QUI\Control
+{
+    /**
+     * constructor
+     *
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        $this->setAttributes([
+            'class'                     => 'quiqqer-simpleContactPerson',
+            'qui-class'                 => 'package/quiqqer/bricks/bin/Controls/SimpleContactWithContactPerson',
+            'labels'                    => true,
+            'data-brickid'              => false,
+            'mailTo'                    => '', // receiver email
+            'showPrivacyPolicyCheckbox' => false,
+            'useCaptcha'                => false,
+            'formContent'               => '',
+            'template'                  => 'default',
+            'textPosition'              => ''
+        ]);
+
+        parent::__construct($attributes);
+
+        $this->setAttribute('cacheable', 0);
+
+        if (!isset($attributes['showPrivacyPolicyCheckbox'])) {
+            $Site = $this->getSite();
+
+            if ($Site) {
+                $this->setAttribute(
+                    'showPrivacyPolicyCheckbox',
+                    $Site->getAttribute('quiqqer.settings.sitetypes.contact.showPrivacyPolicyCheckbox')
+                );
+            }
+        }
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @throws QUI\Exception
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine                = QUI::getTemplateManager()->getEngine();
+        $name                  = '';
+        $email                 = '';
+        $message               = '';
+        $privacyPolicyCheckbox = $this->getAttribute('showPrivacyPolicyCheckbox');
+        $useCaptcha            = $this->getAttribute('useCaptcha');
+        $formContent           = $this->getAttribute('formContent');
+        $error                 = false;
+        $template              = $this->getAttribute('template');
+        $textPosition          = $this->getAttribute('textPosition');
+
+        switch ($template) {
+            case 'standard.formRight':
+                $template = 'standard';
+                break;
+            case 'standard.formLeft':
+                $textPosition = 'quiqqer-simple-contact-textRight__horizontal';
+                $template     = "standard";
+                break;
+            default:
+                $template = "." . $template;
+        }
+
+
+//        $this->addCSSFiles([
+//            dirname(__FILE__) . '/SimpleContactWithContactPerson.css',
+//            dirname(__FILE__) . '/SimpleContactWithContactPerson' . $template . '.css'
+//        ]);
+
+        // Is javascript disabled?
+        if (isset($_POST['name'])
+            && isset($_POST['email'])
+            && isset($_POST['message'])
+        ) {
+            if ($useCaptcha && QUI::getPackageManager()->isInstalled('quiqqer/captcha')) {
+                if (empty($_POST['quiqqer-captcha-response'])
+                    || !CaptchaHandler::isResponseValid($_POST['quiqqer-captcha-response'])) {
+                    $Engine->assign([
+                        'errorMessage' => QUI::getLocale()->get(
+                            'quiqqer/bricks',
+                            'brick.control.simpleContact.error.captcha_failed'
+                        )
+                    ]);
+
+                    $error = true;
+                }
+            }
+
+            //KOM wyswietlenie beldu przy wyslaniu meila, bez klikniecie polityki prywtanosci
+            if ($privacyPolicyCheckbox && empty($_POST['privacyPolicy'])) {
+                $Engine->assign([
+                    'errorMessage' => QUI::getLocale()->get(
+                        'quiqqer/bricks',
+                        'brick.control.simpleContact.error.privacyPolicyRequired'
+                    )
+                ]);
+
+                $error = true;
+            }
+
+            //KOM jesli nie ma bledow, to sprobuj wyslac meila
+            if (!$error) {
+                try {
+                    $this->sendMail($Engine);
+                } catch (\Exception $Exception) {
+                    $Engine->assign([
+                        'errorMessage' => $Exception->getMessage()
+                    ]);
+
+                    $error = true;
+                }
+            }
+        }
+
+        //KOM stworzenie checkboxa polityki pryw.
+        // Privacy Policy checkbox
+        if ($privacyPolicyCheckbox) {
+            $PrivacyPolicySite = $this->getPrivacyPolicySite();
+            $label             = QUI::getLocale()->get(
+                'quiqqer/bricks',
+                'control.simpleContact.privacyPolicy.label'
+            );
+
+            if ($PrivacyPolicySite) {
+                $url = $PrivacyPolicySite->getUrlRewrittenWithHost();
+
+                $label = preg_replace(
+                    '#\[([^\]]*)\]#i',
+                    '<a href="' . $url . '" target="_blank">$1</a>',
+                    $label
+                );
+
+                $Project = QUI::getRewrite()->getProject();
+
+                $Engine->assign([
+                    'projectName' => $Project->getName(),
+                    'projectLang' => $Project->getLang(),
+                    'siteId'      => $PrivacyPolicySite->getId()
+                ]);
+            }
+
+            $label = str_replace(['[', ']'], '', $label);
+
+            $Engine->assign([
+                'privacyPolicyLabel'      => $label,
+                'createPrivacyPolicyLink' => $PrivacyPolicySite !== false
+            ]);
+        }
+
+        //KOM jesli jest zainstalowana paczka "captcha" i wlaczaona opcja, to ja wyswietle
+        // CAPTCHA
+        if ($useCaptcha && QUI::getPackageManager()->isInstalled('quiqqer/captcha')) {
+            $Engine->assign('CaptchaDisplay', new QUI\Captcha\Controls\CaptchaDisplay());
+        }
+
+        if ($error) {
+            $name    = !empty($_POST['name']) ? $_POST['name'] : '';
+            $email   = !empty($_POST['email']) ? $_POST['email'] : '';
+            $message = !empty($_POST['message']) ? $_POST['message'] : '';
+        }
+
+
+        $Engine->assign([
+            'this'         => $this,
+            'name'         => $name,
+            'email'        => $email,
+            'message'      => $message,
+            'formContent'  => $formContent,
+            'textPosition' => $textPosition
+        ]);
+
+        return $Engine->fetch(dirname(__FILE__) . '/SimpleContactWithContactPerson.' . $template . '.html');
+    }
+
+    /**
+     * Send email per post request if javascript is disabled
+     *
+     * @param QUI\Interfaces\Template\EngineInterface $Engine
+     * @throws QUI\Exception
+     */
+    public function sendMail($Engine)
+    {
+        $Site                  = $this->getSite();
+        $privacyPolicyCheckbox = boolval($Site->getAttribute(
+            'quiqqer.settings.sitetypes.contact.showPrivacyPolicyCheckbox'
+        ));
+
+        // email correct?
+        if (!QUI\Utils\Security\Orthos::checkMailSyntax($_POST['email'])) {
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/bricks',
+                    'brick.control.simpleContact.error.wrongEmail'
+                )
+            );
+        }
+
+        $receiver = $this->getAttribute('mailTo');
+        $url      = $this->getProject()->getHost() . $Site->getUrlRewritten();
+
+        // fallback: admin email
+        if (!QUI\Utils\Security\Orthos::checkMailSyntax($receiver)) {
+            $receiver = (QUI::conf('mail', 'admin_mail'));
+        }
+
+        $Mailer = QUI::getMailManager()->getMailer();
+
+        $Mailer->addRecipient($receiver);
+        $Mailer->addReplyTo($_POST['email']);
+        $Mailer->setSubject($Site->getAttribute('title') . ' | ' . $url);
+
+        $body = "
+            <span style=\"font-weight: bold;\">From:</span> {$_POST['name']}<br />
+            <span style=\"font-weight: bold;\">E-mail:</span> {$_POST['email']}<br />
+        ";
+
+        if ($privacyPolicyCheckbox && !empty($_POST['privacyPolicy'])) {
+            $body .= '<span style="font-weight: bold;">';
+            $body .= QUI::getLocale()->get(
+                'quiqqer/bricks',
+                'brick.control.simpleContact.mail.privacyPolicy_accepted'
+            );
+            $body .= '</span><br/>';
+        }
+
+        $body .= "<span style=\"font-weight: bold;\">Message:</span><br /><br />
+            <div style=\"white-space: pre-line;\">{$_POST['message']}
+            </div>";
+
+        $Mailer->setBody($body);
+
+        try {
+            $Mailer->send();
+            $Engine->assign([
+                'successMessage' => QUI::getLocale()->get(
+                    'quiqqer/bricks',
+                    'brick.control.simpleContact.successful'
+                )
+            ]);
+        } catch (\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/bricks',
+                    'brick.control.simpleContact.error.serverError'
+                )
+            );
+        }
+    }
+
+    /**
+     * Get Privacy Policy Site of the current Project
+     *
+     * @return QUI\Projects\Site|false
+     */
+    protected function getPrivacyPolicySite()
+    {
+        try {
+            $Project = QUI::getRewrite()->getProject();
+
+            $result = $Project->getSites([
+                'where' => [
+                    'type' => 'quiqqer/sitetypes:types/privacypolicy'
+                ],
+                'limit' => 1
+            ]);
+        } catch (\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+
+            return false;
+        }
+
+        if (empty($result)) {
+            return false;
+        }
+
+        return current($result);
+    }
+
+    /**
+     * @return mixed|QUI\Projects\Site
+     *
+     * @throws QUI\Exception
+     */
+    public function getSite()
+    {
+        if ($this->getAttribute('Site')) {
+            return $this->getAttribute('Site');
+        }
+
+        $Site = QUI::getRewrite()->getSite();
+
+        $this->setAttribute('Site', $Site);
+
+        return $Site;
+    }
+}
