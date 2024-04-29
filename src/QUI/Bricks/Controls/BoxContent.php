f6f81ec15489731c034d29616a520b5105431bb8
Michael Danielczok <michael@pcsg.de>
2024-04-29 11:49:22 +0000

    feat(BoxContent): improve entries management

--- src/QUI/Bricks/Controls/BoxContent.php
+++ src/QUI/Bricks/Controls/BoxContent.php
@@ -24,9 +24,10 @@ class BoxContent extends QUI\Control
     {
         // default options
         $this->setAttributes([
-            'title' => 'Box Content',
+            'class' => 'quiqqer-boxContent',
             'contentList' => false,
-            'entries' => []
+            'entries' => [],
+            'centerText' => false,
         ]);
 
         parent::__construct($attributes);
@@ -44,52 +45,26 @@ class BoxContent extends QUI\Control
     public function getBody()
     {
         $Engine = QUI::getTemplateManager()->getEngine();
+        $centerText = $this->getAttribute('centerText');
         $entries = $this->getAttribute('entries');
+        $enabledEntries = [];
 
         if (is_string($entries)) {
             $entries = json_decode($entries, true);
         }
 
-        $count = count($entries);
-        $extraClass = '';
+        foreach ($entries as $entry) {
+            if (isset($entry['isDisabled']) && $entry['isDisabled'] === 1) {
+                continue;
+            }
 
-        switch ($count) {
-            case 1:
-                $extraClass = ' box-content-entry-1';
-                break;
-            case 2:
-                $extraClass = ' box-content-entry-2';
-                break;
-            case 3:
-                $extraClass = ' box-content-entry-3';
-                break;
-            case 4:
-                $extraClass = ' box-content-entry-4';
-                break;
-            case 5:
-                $extraClass = ' box-content-entry-5';
-                break;
-            case 6:
-                $extraClass = ' box-content-entry-6';
-                break;
-            case 7:
-                $extraClass = ' box-content-entry-7';
-                break;
-            case 8:
-                $extraClass = ' box-content-entry-8';
-                break;
-            case 9:
-                $extraClass = ' box-content-entry-9';
-                break;
-            case 10:
-                $extraClass = ' box-content-entry-10';
-                break;
+            array_push($enabledEntries, $entry);
         }
 
         $Engine->assign([
             'this' => $this,
-            'entries' => $entries,
-            'extraClass' => $extraClass
+            'entries' => $enabledEntries,
+            'centerText' => $centerText
         ]);
 
         return $Engine->fetch(dirname(__FILE__) . '/BoxContent.html');
